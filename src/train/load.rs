0615f9eeef9928854c94704cba7c64932e9db7ae
Alexdelia <alexandre.delille.57@gmail.com>
2024-04-20 21:33:37 +0200

    save

--- src/train/load.rs
+++ src/train/load.rs
@@ -1,13 +1,24 @@
 use std::{error::Error, path::Path};
+use hmerr::ioe;
 
 pub type Data = Vec<Record>;
 
 pub type Record = (f64, f64);
 
-pub fn parse<P: AsRef<Path>>(path: P) -> hmerr::Result<Data> {
+pub fn parse<P: AsRef<Path>>(path: P) -> hmerr::Result<Data>
+where String: From<P>
+{
 	let mut ret: Data = Vec::new();
 
-	let mut rdr = csv::Reader::from_path(path).map_err(|e| hmerr::Error))
+	let mut rdr = csv::Reader::from_path(&path).map_err(|e| {
+		if !e.is_io_error() {
+			return ioe!(path, e);
+		}
+		match e.into_kind() {
+			csv::ErrorKind::Io(io_error) => ioe!(path, io_error),
+			_ => unreachable!()
+		}
+	})?;
 
 	for result in rdr.deserialize() {
 		let record: Record = result?;
